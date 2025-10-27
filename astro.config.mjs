5a2b6c4480bb72d843423f2d00035222870d18d8
Mycolaos <mycolaos@gmail.com>
2025-10-27 18:55:53 +0100

    added workflow for publishing on github pages; updated config to match gh url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mycolaos.com',
+  site: 'https://mycolaos.github.io',
   integrations: [mdx(), sitemap(), partytown({
     // Example: Add dataLayer.push as a forwarding-event.
     config: {
