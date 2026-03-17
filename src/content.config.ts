b4911309f4cc657fd6d5c915d674e1b95763a9d9
Mycolaos <mycolaos@gmail.com>
2026-03-17 16:45:04 +0100

    added demo section; modified base components and layouts to exclude default global styles - the idea is that demo is using its own styles.

--- src/content.config.ts
+++ src/content.config.ts
@@ -24,20 +24,29 @@ function generateRouteIdIgnoringGroups({ entry, base, data }: GenerateIdOptions)
 		.join('/');
 }
 
+const schema = ({ image }) =>
+	z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: image().optional(),
+		redditUrl: z.string().url().optional(),
+	})
+
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}', generateId: generateRouteIdIgnoringGroups }),
 	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-			redditUrl: z.string().url().optional(),
-		}),
+	schema,
 });
 
-export const collections = { blog };
+const demos = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/demos', pattern: '**/*.{md,mdx}', generateId: generateRouteIdIgnoringGroups }),
+	// Type-check frontmatter using a schema
+	schema,
+});
+
+export const collections = { blog, demos };
